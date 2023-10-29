06388518adce6610a7f9c98335b8cfa6984c18c6
Chiranjiv28 <120295965+Chiranjiv28@users.noreply.github.com>
2023-10-29 11:55:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCjHge5xNNLucrVNqS7TAXXKCIMLEB9oP8",
+  authDomain: "story-teller-app-4a15b.firebaseapp.com",
+  databaseURL: "https://story-teller-app-4a15b-default-rtdb.firebaseio.com",
+  projectId: "story-teller-app-4a15b",
+  storageBucket: "story-teller-app-4a15b.appspot.com",
+  messagingSenderId: "553315492983",
+  appId: "1:553315492983:web:93e7d1fb78aae5f49f9272"
+};
